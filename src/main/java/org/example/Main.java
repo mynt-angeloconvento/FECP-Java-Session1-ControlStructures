69d05513016a7d3e696443ce2d4c6f4a3e1c0542
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-06-30 15:32:20 +0800

    feat: add solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
+
 import java.util.*;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+        String status = myObj.nextLine();
+        System.out.print("Enter age: ");
+        int age = myObj.nextInt();
+        double price = 0;
 
+        switch (status.toLowerCase()) {
+            case "regular":
+                price = age < 18 ? 50 : age >= 18 && age <= 64 ? 100 : 75;
+                break;
+            case "vip":
+                price = age < 18 ? 75 : age >= 18 && age <= 64  ? 150 : 112.5;
+                break;
+            case "premium":
+                price = age < 18 ? 100 : age >= 18 && age <= 64  ? 200 : 150;
+                break;
+            default:
+                System.out.printf("Invalid membership status entered.");
+                break;
+        }
+        if (price != 0) System.out.printf("Price: $%.2f", price);
     }
 }
